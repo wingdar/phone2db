1ffc47d1da4d2460eea08eef1b84cfef0fba9dd3
wingdar <wingdar@gmail.com>
2020-11-06 17:46:22 +0800

    Initial commit 第一次上傳！

---
+++ app/src/main/java/com/wingdar/phone2db/MainActivity.java
@@ -0,0 +1,80 @@
+package com.wingdar.phone2db;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button ok_button = (Button) findViewById(R.id.button);
+        ok_button.setOnClickListener(ok_btn);
+
+        final TextView text_view = (TextView) findViewById(R.id.Text_view);
+
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                MysqlCon con = new MysqlCon();
+                con.run();
+                final String data = con.getData();
+                Log.v("OK",data);
+                text_view.post(new Runnable() {
+                    public void run() {
+                        text_view.setText(data);
+                    }
+                });
+
+            }
+        }).start();
+    }
+
+    private Button.OnClickListener ok_btn = v -> {
+
+        final TextView text_view = (TextView) findViewById(R.id.Text_view);
+
+        Toast toast = Toast.makeText(this, R.string.input_but, Toast.LENGTH_SHORT);
+        toast.show();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // 取得 EditText 資料
+                final EditText edit_text = (EditText) findViewById(R.id.et2);
+                String stringdata = edit_text.getText().toString();
+                final EditText edit_text1 = (EditText) findViewById(R.id.et1);
+                String stringid = edit_text1.getText().toString();
+                // 清空 EditText
+                edit_text.post(new Runnable() {
+                    public void run() {
+                        edit_text.setText("");
+                        edit_text1.setText("");
+                    }
+                });
+                // 將資料寫入資料庫
+                MysqlCon con = new MysqlCon();
+
+                con.insertData(Float.parseFloat(stringdata), stringid );
+                // 讀取更新後的資料
+                final String updata = con.getData();
+                Log.v("OK", updata);
+                text_view.post(new Runnable() {
+                    public void run() {
+                        text_view.setText(updata);
+                    }
+                });
+
+            }
+        }).start();
+    };
+}
---
+++ app/src/main/java/com/wingdar/phone2db/MysqlCon.java
@@ -0,0 +1,83 @@
+package com.wingdar.phone2db;
+
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MysqlCon {
+    // 資料庫定義
+    String mysql_ip = "192.168.1.218";
+    int mysql_port = 3306; // Port 預設為 3306
+    String db_name = "mydb";
+    String url = "jdbc:mysql://"+mysql_ip+":"+mysql_port+"/"+db_name;
+    String db_user = "dar";
+    String db_password = "16584326";
+
+    public void run() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Log.v("DB","加載驅動成功");
+        }catch( ClassNotFoundException e) {
+            Log.e("DB","加載驅動失敗");
+            return;
+        }
+
+        // 連接資料庫
+        try {
+            Connection con = DriverManager.getConnection(url,db_user,db_password);
+            Log.v("DB","遠端連接成功");
+        }catch(SQLException e) {
+            Log.e("DB","遠端連接失敗");
+            Log.e("DB", e.toString());
+        }
+    }
+
+    public String getData() {
+        String data = "";
+        try {
+            Connection con = DriverManager.getConnection(url, db_user, db_password);
+            String sql = "SELECT * FROM TempX";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next())
+            {
+                String id = rs.getString("userid");
+                String temp = rs.getString("tempx");
+                String date = rs.getString("datetime");
+                data += id + ", " + temp + ", 時間："+ date +"\n";
+            }
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public void insertData(float data,String id){
+        try {
+            SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String t = simpleDate.format(new Date());
+            Log.v("時間：",t);
+
+            Connection con = DriverManager.getConnection(url, db_user, db_password);
+//            String sql = "INSERT INTO TempX (datetime, tempx, userid) VALUES ('"+ t +","+ data +","+ id +"')";
+            String sql = "INSERT INTO TempX (datetime, tempx, userid) VALUES ('"+ t +"','"+ data + "','"+ id +"')";
+            Statement st = con.createStatement();
+            st.executeUpdate(sql);
+            st.close();
+            Log.v("DB", "寫入資料完成：" + data);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Log.e("DB", "寫入資料失敗");
+            Log.e("DB", e.toString());
+        }
+    }
+
+}
